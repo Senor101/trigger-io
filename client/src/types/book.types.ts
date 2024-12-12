f43762338bbbe69ed7e812bdfcbab65a076d696a
Senor101 <adikshyanta456@gmail.com>
2024-12-12 21:48:31 +0545

    fix: book service updates

--- client/src/types/book.types.ts
+++ client/src/types/book.types.ts
@@ -1,11 +1,14 @@
 // import { TUser } from './user.types';
 
+import { TUser } from './user.types';
+
 export type TBook = {
   id: string;
   createdAt?: string;
   updatedAt?: string;
   title: string;
   description: string;
-  author: string;
+  author?: TUser;
+  userId?: string;
   ISBN: string;
 };
